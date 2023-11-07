8f5b65675f657892f58d23d95aa335d7298d5cd7
MarcuccioArme <121099587+MarcuccioArme@users.noreply.github.com>
2023-11-07 09:43:11 +0100

    Aggiunti file
    Aggiunto secondo esercizio (inizio)

---
+++ Eserc_5 Data&Ora/a.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "<table border=1>";
+    echo "<tr><th colspan=7>" . date("F") . "</th></tr>";
+    echo "<tr>
+        <td width=50 align=center> Sun </td>
+        <td width=50 align=center> Mon </td>
+        <td width=50 align=center> Tue </td>
+        <td width=50 align=center> Wed </td>
+        <td width=50 align=center> Thu </td>
+        <td width=50 align=center> Fri </td>
+        <td width=50 align=center> Sat </td>
+        </tr>";
+
+?>
